fa44c204c70d20f7fade9b2d25af04ba5a7030d4
tangmj123 <1056617205@qq.com>
2016-04-09 15:10:47 +0800

    update

--- src/test/java/com/madhosue/java8/stream/Stream1.java
+++ src/test/java/com/madhosue/java8/stream/Stream1.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Before;
